806011ad3f280c64338c94f220be34e2460aa8c3
jimin <Jimn69@github.com>
2025-06-30 18:54:28 +0200

    Overriding 1.8.9's chat char limit to match 1.13+ versions of the game

--- src/main/java/com/pwetutils/mixins/KoreanInputMixin.java
+++ src/main/java/com/pwetutils/mixins/KoreanInputMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiChat.class)
-public class GuiChatMixin {
+public class KoreanInputMixin {
     @Shadow
     protected GuiTextField inputField;
 
